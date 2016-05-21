e523baff0f433844c6aa3e7862b350ec5278f249
Ethan Randall <iJunkie22@gmail.com>
2016-05-20 20:20:50 -0400

    filled in name_table w/ cursorNameMap

--- xcurnames.py
+++ xcurnames.py
@@ -1,35 +1,60 @@
 nXCUR = 'this-xcur-filename-is-undefined'
 ukCNAME = 'UnknownMouseCapeCursorName'
 
+
 name_table = [
-    ('com.apple.coregraphics.Alias',    'Alias',        'alias'),
-    ('com.apple.coregraphics.Arrow',    'Arrow',        'left-main'),
-    ('com.apple.coregraphics.Copy',     'Copy',         nXCUR),
-    ('com.apple.coregraphics.IBeam',    'IBeam',        'horizontal-text'),
-    ('com.apple.cursor.3',              ukCNAME,        nXCUR),
-    ('com.apple.cursor.4',              ukCNAME,        nXCUR),
-    ('com.apple.cursor.5',              ukCNAME,        nXCUR),
-    ('com.apple.cursor.7',              ukCNAME,        nXCUR),
-    ('com.apple.cursor.11',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.13',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.18',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.19',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.21',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.23',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.24',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.30',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.32',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.34',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.36',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.38',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.39',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.40',             ukCNAME,        nXCUR),
-    ('com.apple.cursor.41',             ukCNAME,        nXCUR)
+    ('com.apple.coregraphics.Alias',    'Alias',            'alias'),
+    ('com.apple.coregraphics.Arrow',    'Arrow',            'left-main'),
+    ('com.apple.coregraphics.Copy',     'Copy',             nXCUR),
+    ('com.apple.coregraphics.Move',     'Move',             nXCUR),
+    ('com.apple.coregraphics.Wait',     'Wait',             nXCUR),
+    ('com.apple.coregraphics.Empty',    'Empty',            nXCUR),
+    ('com.apple.coregraphics.IBeam',    'IBeam',            'horizontal-text'),
+    ('com.apple.coregraphics.IBeamXOR', 'IBeamXOR',         nXCUR),
+    ('com.apple.coregraphics.ArrowCtx', 'Ctx Arrow',        nXCUR),
+    ('com.apple.cursor.2',              'Link',             nXCUR),
+    ('com.apple.cursor.3',              'Forbidden',        nXCUR),
+    ('com.apple.cursor.4',              'Busy',             nXCUR),
+    ('com.apple.cursor.5',              'Copy Drag',        nXCUR),
+    ('com.apple.cursor.7',              'Crosshair',        nXCUR),
+    ('com.apple.cursor.8',              'Crosshair 2',      nXCUR),
+    ('com.apple.cursor.9',              'Camera 2',         nXCUR),
+    ('com.apple.cursor.10',             'Camera',           nXCUR),
+    ('com.apple.cursor.11',             'Closed',           nXCUR),
+    ('com.apple.cursor.12',             'Open',             nXCUR),
+    ('com.apple.cursor.13',             'Pointing',         nXCUR),
+    ('com.apple.cursor.14',             'Counting Up',      nXCUR),
+    ('com.apple.cursor.15',             'Counting Down',    nXCUR),
+    ('com.apple.cursor.16',             'Counting Up/Down', nXCUR),
+    ('com.apple.cursor.17',             'Resize W',         nXCUR),
+    ('com.apple.cursor.18',             'Resize E',         nXCUR),
+    ('com.apple.cursor.19',             'Resize W-E',       nXCUR),
+    ('com.apple.cursor.20',             'Cell XOR',         nXCUR),
+    ('com.apple.cursor.21',             'Resize N',         nXCUR),
+    ('com.apple.cursor.22',             'Resize S',         nXCUR),
+    ('com.apple.cursor.23',             'Resize N-S',       nXCUR),
+    ('com.apple.cursor.24',             'Ctx Menu',         nXCUR),
+    ('com.apple.cursor.25',             'Poof',             nXCUR),
+    ('com.apple.cursor.26',             'IBeamH.',          nXCUR),
+    ('com.apple.cursor.27',             'Window E',         nXCUR),
+    ('com.apple.cursor.28',             'Window E-W',       nXCUR),
+    ('com.apple.cursor.29',             'Window NE',        nXCUR),
+    ('com.apple.cursor.30',             'Window NE-SW',     nXCUR),
+    ('com.apple.cursor.31',             'Window N',         nXCUR),
+    ('com.apple.cursor.32',             'Window N-S',       nXCUR),
+    ('com.apple.cursor.33',             'Window NW',        nXCUR),
+    ('com.apple.cursor.34',             'Window NW-SE',     nXCUR),
+    ('com.apple.cursor.35',             'Window SE',        nXCUR),
+    ('com.apple.cursor.36',             'Window S',         nXCUR),
+    ('com.apple.cursor.37',             'Window SW',        nXCUR),
+    ('com.apple.cursor.38',             'Window W',         nXCUR),
+    ('com.apple.cursor.39',             'Resize Square',    nXCUR),
+    ('com.apple.cursor.40',             'Help',             nXCUR),
+    ('com.apple.cursor.41',             'Cell',             nXCUR),
+    ('com.apple.cursor.42',             'Zoom In',          nXCUR),
+    ('com.apple.cursor.43',             'Zoom Out',         nXCUR)
 ]
 
 
-def mc_to_plist(mouse_cape_cursor_name):
-    for line in name_table:
-        if line[1] == mouse_cape_cursor_name:
-            return line[0]
-    return None
+cursor_name_map = {k: v for (v, k, f) in name_table}
+
