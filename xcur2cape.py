e70477db940e120ad67b3bf374925eb4aba07e95
Ethan Randall <iJunkie22@gmail.com>
2016-05-22 15:45:03 -0400

    added support for animated icons!

--- xcur2cape.py
+++ xcur2cape.py
@@ -79,7 +79,7 @@ class CapeCursor(object):
     def export_dict(self):
         d2 = {}
         d2['FrameCount'] = self.frame_count
-        d2['FrameDuration'] = self.frame_duration
+        d2['FrameDuration'] = 1.000 / float(self.frame_count)
         d2['HotSpotX'] = self.hotspotx
         d2['HotSpotY'] = self.hotspoty
         d2['PointsHigh'] = self.height
@@ -101,6 +101,7 @@ class CapeCursor(object):
                     new_cc.hotspoty = float(xc.hs_y)
                     new_cc.height = float(xc.img_size)
                     new_cc.width = float(xc.img_size)
+                    new_cc.frame_count = xc.frame_count
             new_cc.mc_namekey = line[1]
             yield new_cc
 
@@ -134,10 +135,25 @@ class XCursorSet(object):
     def is_known_xc(self):
         return self.xc_name in [x[2] for x in xcurnames.name_table]
 
-    def unpack_from_conf(self):
-        for line in self.conf_file_str.splitlines():
-            if not line.startswith("#"):
-                self.xcursors.append(XCursor.from_conf_file_line(self.conf_fp, line))
+    def unpack_from_conf(self, animated=False):
+        if not animated:
+            for line in self.conf_file_str.splitlines():
+                if not line.startswith("#"):
+                    self.xcursors.append(XCursor.from_conf_file_line(self.conf_fp, line))
+
+        else:  # if animated:
+            conf_line_groups = {}
+            conf_line_keys = conf_line_groups.viewkeys()
+
+            for line in self.conf_file_str.splitlines():
+                if not line.startswith("#"):
+                    conf_size = line.partition('\t')[0]
+                    if conf_size not in conf_line_keys:
+                        conf_line_groups[conf_size] = []
+                    conf_line_groups[conf_size].append(line)
+
+            for k in conf_line_keys:
+                self.xcursors.append(XCursor.animated_from_conf_file_lines(self.conf_fp, conf_line_groups[k]))
 
     @classmethod
     def from_conf_file(cls, conf_fp):
@@ -147,7 +163,8 @@ class XCursorSet(object):
         with open(new_xcs.conf_fp, 'r') as conf_fd:
             new_xcs.conf_file_str = conf_fd.read()
 
-        new_xcs.unpack_from_conf()
+        is_animated = xcurnames.name_table[[n[2] for n in xcurnames.name_table].index(new_xcs.xc_name)][3]
+        new_xcs.unpack_from_conf(animated=is_animated)
         return new_xcs
 
     @classmethod
@@ -168,6 +185,7 @@ class XCursor(object):
         self.hs_y = 0
         self.fp = ""
         self.img_data = ""
+        self.frame_count = 1
 
     @classmethod
     def from_conf_file_line(cls, conf_fp, conf_line_str):
@@ -192,15 +210,61 @@ class XCursor(object):
         new_xc.hs_y = new_xc.hs_y if new_xc.hs_y <= new_xc.img_size else (new_xc.img_size / 2.0)
         new_xc.hs_x = new_xc.hs_x if (new_xc.hs_x / new_xc.img_size) != (15.0/16.0) else (new_xc.img_size / 2.0)
         new_xc.hs_y = new_xc.hs_y if (new_xc.hs_y / new_xc.img_size) != (15.0/16.0) else (new_xc.img_size / 2.0)
+        return new_xc
+
+    @classmethod
+    def animated_from_conf_file_lines(cls, conf_fp, conf_lines_list):
+        new_xc = cls()
+        assert isinstance(conf_lines_list, list)
+        conf_img_size, conf_hs_x, conf_hs_y, sample_fp, trash = str(conf_lines_list[0]).split()
+        del trash
+        if not os.path.isabs(sample_fp):
+            sample_fp = os.path.normpath(os.path.join(conf_fp, sample_fp))
+        sample_img_header = None
+        with open(sample_fp, 'rb') as sample_fd:
+            sample_img_header = sample_fd.read()[16:][:8]
+
+        sample_x1, sample_y1 = struct.unpack('>ii', sample_img_header)
+        sample_img_size = sample_x1
+        frame_fns = [conf_line.split()[3] for conf_line in conf_lines_list]
+        frame_fns = map(lambda x: x if os.path.isabs(x) else os.path.normpath(os.path.join(conf_fp, x)), frame_fns)
+        frame_fns_quo = ["\'" + z + "\'" for z in frame_fns]
+        frame_fns_str = ' '.join(frame_fns_quo)
+        arg_frmt = "montage {0} -tile 1x -background 'transparent' -geometry '{1}x{1}+0+0>' '{2}'"
+        ripped_dir = os.path.abspath(os.path.dirname(sample_fp))
+        export_fp = os.path.join(ripped_dir, 'JOINED_' + os.path.basename(sample_fp))
+        arg_str = arg_frmt.format(frame_fns_str, sample_img_size, export_fp)
+        p2 = subprocess.Popen(arg_str, shell=True, stdout=subprocess.PIPE)
+        p2.wait()
+
+        new_xc.fp = export_fp
+        with open(export_fp, 'rb') as export_fd:
+            new_xc.img_data = base64.b64encode(export_fd.read())
+
+        new_xc.hs_x = int(conf_hs_x)
+        new_xc.hs_y = int(conf_hs_y)
+        new_xc.img_size = sample_x1
+        new_xc.hs_x = (float(new_xc.hs_x) / float(conf_img_size)) * new_xc.img_size
+        new_xc.hs_y = (float(new_xc.hs_y) / float(conf_img_size)) * new_xc.img_size
+        new_xc.hs_x = new_xc.hs_x if new_xc.hs_x <= new_xc.img_size else (new_xc.img_size / 2.0)
+        new_xc.hs_y = new_xc.hs_y if new_xc.hs_y <= new_xc.img_size else (new_xc.img_size / 2.0)
+        new_xc.hs_x = new_xc.hs_x if (new_xc.hs_x / new_xc.img_size) != (15.0/16.0) else (new_xc.img_size / 2.0)
+        new_xc.hs_y = new_xc.hs_y if (new_xc.hs_y / new_xc.img_size) != (15.0/16.0) else (new_xc.img_size / 2.0)
+        new_xc.frame_count = len(conf_lines_list)
 
         return new_xc
 
 
-test_xct1 = XCursorTheme.unpack_theme(os.path.expanduser('~/Downloads/Breeze-Obsidian/'))
-print test_xct1.theme_name
-print len(test_xct1.cursors)
-test_co1 = CapeObject.from_theme(test_xct1)
+def run_tool(theme_fp):
+    test_xct1 = XCursorTheme.unpack_theme(theme_fp)
+    print test_xct1.theme_name
+    print len(test_xct1.cursors)
+    test_co1 = CapeObject.from_theme(test_xct1)
+    test_pl_dict = test_co1.export_dict()
+    plistlib.writePlist(test_pl_dict, test_pl_dict['Identifier'] + '.cape')
 
-test_pl_dict = test_co1.export_dict()
-plistlib.writePlist(test_pl_dict, test_pl_dict['Identifier'] + '.cape')
+
+run_tool(os.path.expanduser('~/Downloads/Breeze-Obsidian/'))
+run_tool(os.path.expanduser('~/Downloads/Breeze-Snow/'))
+run_tool(os.path.expanduser('~/Downloads/Breeze-Hacked/'))
 
